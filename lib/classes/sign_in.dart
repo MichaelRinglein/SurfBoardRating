361e590fa8202aecb6f2fbf78ee275650cb2465b
Michael Ringlein <michael.ringlein@gmail.com>
2020-11-01 14:38:12 +0800

    login screen

--- lib/classes/sign_in.dart
+++ lib/classes/sign_in.dart
@@ -13,35 +13,55 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login In / Sign Up'),
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color:  Color(0xFFE8F8FA),
+            blurRadius: 20,
+            spreadRadius: 0,
+          ),
+        ],
+        image: DecorationImage(
+          image: AssetImage(
+            'waves-login.png'
+          ),
+          fit: BoxFit.contain,
+          alignment: AlignmentDirectional.bottomCenter,
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
-              child: Text('Login Anonymously'),
-              onPressed: () async {
-                await _auth.loginAnonym();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyHomePage(title: 'Successfully logged in')),
-                );
-              },
-            ),
-            RaisedButton(
-              child: Text('Login with Google'),
-              onPressed: () async {
-                await _auth.loginWithGoogle();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyHomePage(title: 'Successfully logged in')),
-                );
-              },
-            ),
-          ],
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Color(0xFFA8DFE5),
+          title: Text('Login In / Sign Up'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text('Login Anonymously'),
+                onPressed: () async {
+                  await _auth.loginAnonym();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyHomePage(title: 'Successfully logged in')),
+                  );
+                },
+              ),
+              RaisedButton(
+                child: Text('Login with Google'),
+                onPressed: () async {
+                  await _auth.loginWithGoogle();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyHomePage(title: 'Successfully logged in')),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
