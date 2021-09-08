da448ac624d24f612ee3f9d85b10a0abe5be6c89
Michael <michael.ringlein@gmail.com>
2021-09-08 15:47:06 +0200

    refactor: FutureBuilder to StreamBuilder

--- lib/classes/ratings_display.dart
+++ lib/classes/ratings_display.dart
@@ -23,18 +23,26 @@ class _RatingDisplayState extends State<RatingDisplay> {
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _ratings = FirebaseFirestore.instance
+        .collection('ratings')
+        .doc('surfboards')
+        .collection(widget.surfboard)
+        .snapshots();
+
     final user = Provider.of<User>(context);
-    CollectionReference _ratings =
-        FirebaseFirestore.instance.collection('ratings');
+    //CollectionReference _ratings =
     //double _size = 40;
     if (user != null) {
       // todo: StreamBuilder instead of FutureBuilder https://firebase.flutter.dev/docs/firestore/usage/#realtime-changes
-      return FutureBuilder<QuerySnapshot>(
-          future: _ratings.doc('surfboards').collection(widget.surfboard).get(),
+      return StreamBuilder<QuerySnapshot>(
+          stream: _ratings,
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasError) {
-              return Text('something went wrong');
+              return Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading...');
             }
             if (snapshot.connectionState == ConnectionState.done) {
               _amountRatings = snapshot.data.docs.length;
@@ -43,48 +51,57 @@ class _RatingDisplayState extends State<RatingDisplay> {
                 _averageRating = _rating / _amountRatings;
               });
 
-              try {
-                //_rating != null ? _rating = data['rating'] : _rating = 0;
-                //print('_rating now is $_rating');
-                //print('data is $data');
-                return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.star,
-                            size: 20,
-                            color: Colors.orange,
-                          ),
-                          Text(_averageRating.toStringAsFixed(2) +
-                              ' (' +
-                              _amountRatings.toString() +
-                              ')'),
-                        ],
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.max,
+                children: snapshot.data.docs.map((DocumentSnapshot document) {
+                  return Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 20,
+                        color: Colors.orange,
                       ),
-                    ]);
-              } catch (e) {
-                print(e.toString());
-                return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Column(
-                        children: [
-                          Text('Rating could not be loaded'),
-                        ],
-                      )
-                    ]);
-              }
+                      Text(_averageRating.toStringAsFixed(2) +
+                          ' (' +
+                          _amountRatings.toString() +
+                          ')'),
+                    ],
+                  );
+                }).toList(),
+              );
             }
             if (snapshot.connectionState == ConnectionState.none) {
               return Container();
             }
-            return Text('Loading');
+
+            _amountRatings = snapshot.data.docs.length;
+            snapshot.data.docs.forEach((doc) {
+              _rating += doc['rating'];
+              _averageRating = _rating / _amountRatings;
+            });
+
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisSize: MainAxisSize.max,
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                return Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      size: 20,
+                      color: Colors.orange,
+                    ),
+                    Text(_averageRating.toStringAsFixed(2) +
+                        ' (' +
+                        _amountRatings.toString() +
+                        ')'),
+                  ],
+                );
+              }).toList(),
+            );
           });
     } else {
       return Row(
